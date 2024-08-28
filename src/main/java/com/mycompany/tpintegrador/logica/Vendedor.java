f0c837116b418c84dd2ac05134efb50200696a28
Marcos Poet <marcospoet@gmail.com>
2024-08-28 01:58:53 -0300

    Agregue a vendedor el método “distancia” que recibe un Cliente como argumento y calcula la distancia en kilómetros entre ellos (usando una implementación de la fórmula de Haversine).

--- src/main/java/com/mycompany/tpintegrador/logica/Vendedor.java
+++ src/main/java/com/mycompany/tpintegrador/logica/Vendedor.java
@@ -32,6 +32,27 @@ public class Vendedor {
         }
         return null;
     }
+
+    public double distancia(Cliente cliente) {
+        //la idea salio de https://gist.github.com/vananth22/888ed9a22105670e7a4092bdcf0d72e4
+
+        final int R = 6371; // Radio de la Tierra en kilómetros
+
+        double lat1 = this.coordenada.getLat();
+        double lon1 = this.coordenada.getLat();
+        double lat2 = cliente.getCoordenada().getLat();
+        double lon2 = cliente.getCoordenada().getLat();
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c; // Convertir a kilómetros
+
+        return distance;
+    }
     
     public int getId() {
         return id;
